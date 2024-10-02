5166216e305673f1ca9de459a2282ebf8e0243c7
Refanda Surya Saputra <refandasuryasaputra@gmail.com>
2024-10-02 20:18:26 +0700

    initial commit

---
+++ src/main/java/com/refanzzzz/tokonyadia/TokonyadiaApplication.java
@@ -0,0 +1,12 @@
+package com.refanzzzz.tokonyadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokonyadiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokonyadiaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/refanzzzz/tokonyadia/TokonyadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refanzzzz.tokonyadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokonyadiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
